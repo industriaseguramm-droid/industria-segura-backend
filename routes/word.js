1b34b3624fbfe43a31e44a84785907107e4f5592
industriaseguramm-droid <industriaseguramm@gmail.com>
2026-04-28 10:00:05 -0600

    Agregar rutas para Acta Constitutiva y Responsiva

--- routes/word.js
+++ routes/word.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const { generarResumenWord } = require('../controllers/word.controller');
+const { generarResumenWord, generarActaConstitutiva, generarResponsiva } = require('../controllers/word.controller');
 const { verificarToken } = require('../middleware/auth');
 
-// GET /api/expedientes/:id/generar-resumen
-router.get('/:id/generar-resumen', verificarToken, generarResumenWord);
+router.get('/:id/generar-resumen',    verificarToken, generarResumenWord);
+router.get('/:id/generar-acta',       verificarToken, generarActaConstitutiva);
+router.get('/:id/generar-responsiva', verificarToken, generarResponsiva);
 
 module.exports = router;
